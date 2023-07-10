761bc88790e644d335ef57e7cff5b19fa791d7a4
SerkovaE <katti107@mail.ru>
2023-07-10 18:03:39 +0300

    add "task 22"

--- task22.py
+++ task22.py
@@ -3,28 +3,17 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 
-# n = int(input("Введите количество элементов 1 множества. n = "))
-# m = int(input("Введите количество элементов 2 множества. m = "))
-# def Set():
-#     for i in range():
-#         SetN.add(int(input()))
-#         return (SetN)
-# print("Введите числа первого множества:")
-# SetN = int(input(Set(n)))
-# set.update([input()])
-SetN = {}
-SetN.add(int(input()))
-# Set()4
-# SetM = {}
-# Sett = {}
-
-
-# from random import randint
-# list_1 = [randint(0, 10) for _ in range(int(input('Введите размер первого набора чисел:  ')))]
-# list_2 = [randint(0, 10) for _ in range(int(input('Введите размер второго набора чисел:  ')))]
-# print(list_1)
-# print(list_2)
-# set_1 = set(list_1)
-# set_2 = set(list_2)
-# list_final = sorted(set_1.intersection(set_2))
-# print(list_final)
+n = int(input('Введите количество элементов первого набора чисел: '))
+m = int(input('Введите количество элементов второго набора чисел: '))
+arr1 = []
+arr2 = []
+for i in range(n):
+    arr1.append(int(input('Введите элемент первого массива: ')))
+for i in range(m):
+    arr2.append(int(input('Введите элемент второго массива: ')))
+arr3 = []
+for i in arr1:
+    if i in arr2 and i not in arr3:
+            arr3.append(i)
+arr3.sort()
+print(arr3)
