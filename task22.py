3a28163b839399fd727bdf028002b10376fd5d9f
SerkovaE <katti107@mail.ru>
2023-07-10 17:51:34 +0300

    add "task 24"

---
+++ task22.py
@@ -0,0 +1,30 @@
+# Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с повторениями).
+# Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа. n — кол-во элементов первого множества.
+# m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
+
+# n = int(input("Введите количество элементов 1 множества. n = "))
+# m = int(input("Введите количество элементов 2 множества. m = "))
+# def Set():
+#     for i in range():
+#         SetN.add(int(input()))
+#         return (SetN)
+# print("Введите числа первого множества:")
+# SetN = int(input(Set(n)))
+# set.update([input()])
+SetN = {}
+SetN.add(int(input()))
+# Set()4
+# SetM = {}
+# Sett = {}
+
+
+# from random import randint
+# list_1 = [randint(0, 10) for _ in range(int(input('Введите размер первого набора чисел:  ')))]
+# list_2 = [randint(0, 10) for _ in range(int(input('Введите размер второго набора чисел:  ')))]
+# print(list_1)
+# print(list_2)
+# set_1 = set(list_1)
+# set_2 = set(list_2)
+# list_final = sorted(set_1.intersection(set_2))
+# print(list_final)
